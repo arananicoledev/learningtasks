af9f75ecb0d026000163cc3ff27243a1a3824ea3
arananicoledev <arananicole88@gmail.com>
2026-06-16 12:22:04 +0800

    Create mission-vision.php

---
+++ php/mission-vision.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>Gallery</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+
+        <header>
+        <h1>Gallery</h1>
+        </header>
+
+        <nav>
+            <a href="index.html">Home</a>
+            <a href="about-us.html">About Us</a>
+            <a href="mission-vision.html">Mission & Vision</a>
+            <a href="gallery.html">Gallery</a>
+        </nav>
+
+        <div class="container">
+        
+        <div class="gallery">
+            <img src="images/office.jpg" alt="">
+            <img src="images/team.jpg" alt="">
+            <img src="images/technology.jpg" alt="">
+            <img src="images/project1.jpg" alt="">
+            <img src="images/project2.jpg" alt="">
+            <img src="images/project3.jpg" alt="">
+        </div>
+
+        </div>
+
+
+
+    </body>
+</html>

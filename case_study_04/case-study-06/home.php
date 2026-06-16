0169e0ad646c058ecca20f5523acaa2b48b7927a
arananicoledev <arananicole88@gmail.com>
2026-06-16 13:13:51 +0800

    Create home.php

---
+++ case_study_04/case-study-06/home.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+
+    <h1>My Website</h1>
+
+    <nav>
+        <a href="index.html">Home</a> /
+        <a href="projects.html">Projects</a> /
+        <a href="jobs.html">Job Postings</a> /
+        <a href="testimonies.html">Testimonies</a> /
+        <a href="contact.html">Contact Us</a> /
+    </nav>
+
+    <hr>
+
+    <h2>Latest News</h2>
+
+    <h3>Mindanao Quake Aftermath</h3>
+    <p> A magnitude 7.8 earthquake that struck off the coast of Sarangani killed 61 people, 
+        injured hundreds, and caused over P1 billion in infrastructure damage. 
+        Authorities are struggling to reach isolated communities and assess damage to thousands of classrooms. </p>
+
+    <h3>Middle East Conflict Negotiations</h3>
+    <p>Diplomatic efforts and a potential peace deal are dominating international headlines, 
+        with Donald Trump announcing an agreement to permanently end military action, 
+        though pushback and details regarding the timeline remain a major point of discussion</p>
+
+    <h2>Upcoming Events</h2>
+
+    <ul>
+        <li>June 28, 2026 - Music Travel Love Philippine Tour</li>
+        <li>July 5, 2026 - EXO Live in Manila</li>
+        <li>July 15, 2026 - Daya Asia Tour</li>
+    </ul>
+
+    </body>
+</html>

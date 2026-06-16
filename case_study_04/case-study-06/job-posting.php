1e5902eefc939fbe88e9201952e571e6e4fb4d9d
arananicoledev <arananicole88@gmail.com>
2026-06-16 13:14:25 +0800

    Create job-posting.php

---
+++ case_study_04/case-study-06/job-posting.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Job Postings</title>
+</head>
+<body>
+
+<h1>Job Postings</h1>
+
+<nav>
+    <a href="index.php">Home</a> /
+    <a href="home.php">Home</a> /
+    <a href="projects.php">Projects</a> /
+    <a href="jobs.php">Job Postings</a> /
+    <a href="testimonies.php">Testimonies</a> /
+    <a href="contact.php">Contact Us</a> /
+</nav>
+
+<hr>
+
+<ul>
+    <li>Web Developer</li>
+    <li>Software Engineer</li>
+    <li>Graphic Designer</li>
+    <li>Network Administrator</li>
+    <li>Database Administrator</li>
+    <li>System Analyst</li>
+    <li>Technical Support Specialist</li>
+    <li>Project Manager</li>
+</ul>
+
+</body>
+</html>

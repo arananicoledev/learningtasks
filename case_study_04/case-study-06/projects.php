2f8668d6e3a195c4039bbf06ecf106b52e2896a5
arananicoledev <arananicole88@gmail.com>
2026-06-16 13:14:08 +0800

    Create projects.php

---
+++ case_study_04/case-study-06/projects.php
@@ -0,0 +1,55 @@
+<html>
+    <head>
+        <title>Gallery</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+
+    <header>
+        <h1>Gallery</h1>
+    </header>
+
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="about-us.php">About Us</a>
+        <a href="mission-vision.php">Mission & Vision</a>
+        <a href="contact-us.php">Contact Us</a>
+        <a href="projects.php">Projects</a>
+
+   <div class="gallery">
+
+        <div class="Office">
+            <img src="images/office.jpg" alt="Office">
+            <p>Project 1</p>
+        </div>
+
+        <div class="Team">
+            <img src="images/team.jpg" alt="Team">
+            <p>Project 2</p>
+        </div>
+
+        <div class="Technology">
+            <img src="images/technology.jpg" alt="Technology">
+        </div>
+
+        <div class="Project 1">
+            <img src="images/project1.jpg" alt="Project 1">
+            <p>Project 4</p>
+        </div>
+
+        <div class="Project 2">
+            <img src="images/project2.jpg" alt="Project 2">
+            <p>Project 5</p>
+        </div>
+
+        <div class="Project 3">
+            <img src="images/project3.jpg" alt="Project 3">
+            <p>Project 6</p>
+        </div>
+
+   
+
+    </div>
+
+</body>
+</html>

95920a72358481d67722b04cd210a8cfec7d6b55
arananicoledev <arananicole88@gmail.com>
2026-06-16 12:22:48 +0800

    Create about-us.php

---
+++ php/about-us.php
@@ -0,0 +1,41 @@
+
+    <html>
+        <head>
+            <title>About Us</title>
+            <link rel="stylesheet" href="css/style.css">
+        </head>
+        <body>
+
+        <header>
+        <h1>About Us</h1>
+        </header>
+
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="about_us.php">About Us</a>
+            <a href="mission-vision.php">Mission & Vision</a>
+            <a href="gallery.php">Gallery</a>
+        </nav>
+
+        <div class="container">
+
+        <div class="card">
+        <h2>MY FIRST WEB PAGE</h2>
+            <p>
+                Founded in 2020, BlueTech Solutions provides reliable technology services
+                to organizations and businesses. Our team focuses on innovation,
+                quality, and customer satisfaction.
+            </p>
+
+        <h2>Core Values</h2>
+
+        </div>
+
+        </div>
+
+        <footer>
+            <p>MY FIRST WEB PAGE</p>
+        </footer>
+
+    </body>
+</html>

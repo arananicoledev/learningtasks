1964c52cd234d3bfc3c1c4a7c15c5431bfc763b7
arananicoledev <arananicole88@gmail.com>
2026-06-17 13:11:01 +0800

    Create index.php

---
+++ activity_05/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Activity 05</title>
+    <link rel= "stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <h1>My Website</h1>
+        </div>
+
+        <div class="container">
+
+        </div>
+        
+        <div class="footer"></div>
+    </div>
+</body>
+</html>
